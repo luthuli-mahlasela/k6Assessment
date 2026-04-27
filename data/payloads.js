a78abdee5a1bc0573e3ac13703a706e4a12330f8
luthulim <luthuli_mahlasela@yahoo.com>
2026-04-27 22:23:20 +0200

    corrected naming the testimonial payload

--- data/payloads.js
+++ data/payloads.js
@@ -10,7 +10,7 @@ export const PAYLOADS = {
          "rating": 5,
          "isPublic": true
     },
-    updatetesimonial: {
+    updatetestimonials: {
         "title": "string1",
          "content": "string1",
          "rating": 1,
