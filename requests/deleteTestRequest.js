6a4d63e49e985232e9c5d81cb9690cd7a48a0a49
luthulim <luthuli_mahlasela@yahoo.com>
2026-04-28 21:56:59 +0200

    added delete payload request and test to delete the testimonial

---
+++ requests/deleteTestRequest.js
@@ -0,0 +1,11 @@
+import http from 'k6/http';
+import { HEADERS } from '../Config/constants.js';
+import { URLs } from '../Config/Urls.js';
+
+export function deleteTestimonialRequest(token,testimonialId){
+
+    const url = `${URLs.testimonials}/${testimonialId}`;
+    const headers = token ? {...HEADERS.json,Authorization:`Bearer ${token}` }:HEADERS.json;
+
+    return http.del(url,{headers});
+};
---
+++ tests/deleteTestimonialTest.js
@@ -0,0 +1,29 @@
+import { TEST_Config } from "../Config/constants.js";
+import { deleteTestimonialRequest } from "../requests/deleteTestRequest.js";
+import { loginRequest } from "../requests/authRequest.js";
+//import { validateUpdateTestimonialResponse } from '../Validators/updateTestimonialValidator.js';
+import { addTestimonial } from "../requests/testimonialRequest.js";
+import { PAYLOADS } from "../data/payloads.js";
+
+
+export const options = {
+    vus: TEST_Config.vus,
+    duration: TEST_Config.duration
+};
+
+export default function (){
+    const loginResponse = loginRequest(PAYLOADS.login);     
+    const body = loginResponse.json();
+    const token = body.data.token;
+
+    const createResponse = addTestimonial(token, PAYLOADS.testimonials);
+    const testimonialId = createResponse.json().data.Id;    
+
+    const response = deleteTestimonialRequest(token,testimonialId);
+    
+    console.log(`Response status: ${response.status}`);
+    console.log(`Response body: ${response.body}`);         
+ // validateUpdateTestimonialResponse(response);
+
+
+}   
